51b90d06cc67e416730b03aadebc7bf6ab7fc606
bilaluali <bilaluali98@gmail.com>
2021-09-29 22:16:57 +0200

    Defined HousesOverview component

---
+++ src/modules/house/components/HousesOverview/HousesOverview.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {  Grid } from '@material-ui/core';
+import classnames from 'classnames';
+
+import HouseCard from '../HouseCard';
+import useStyles from './styles';
+
+const HousesOverview = ({
+    className,
+    items,
+    ...props
+}) => {
+    const classes = useStyles();
+    return (
+        <Grid
+            {...props}
+            className={classnames(className, classes.root)}
+            spacing={2}
+            container
+        >
+            {items.map(item => 
+                <Grid
+                    item
+                    key={item.id}
+                    xs={12}
+                    md={6}
+                    lg={3}
+                >
+                    <HouseCard {...item}/>
+                </Grid>
+            )}
+            
+        </Grid>
+    )
+};
+
+export default HousesOverview;
---
+++ src/modules/house/containers/HouseCard.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import HouseCard from '../components/HouseCard';
+
+
+const HouseCardContainer = (props) => {
+    return (
+        <HouseCard {...props} />
+    );
+};
+
+export default HouseCardContainer;
