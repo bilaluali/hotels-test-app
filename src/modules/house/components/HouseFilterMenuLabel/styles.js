eb1aa35f0087d93b007b40f705fd9692b63b0707
bilaluali <bilaluali98@gmail.com>
2021-09-30 13:34:38 +0200

    Defined HouseFilterMenuLabel component

---
+++ src/modules/house/components/HouseFilterMenuLabel/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        '& .MuiListItemText-primary': {
+            color: `${theme.palette.success.main} !important`
+        },
+        '& .MuiListItemAvatar-root': {
+            minWidth: 'auto',
+        },
+        padding: [[4, 0, 4, 0]]
+    },
+    avatar: {
+        marginLeft: 5,
+        color: theme.palette.success.main
+    },
+    text: {
+        margin: 0,
+    }
+}));
+
+export default useStyles;
